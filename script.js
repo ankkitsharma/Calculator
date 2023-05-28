dc32aec04dcf3078774bfb7ac646f3efbf1057d1
Ankit Sharma <ankitsharma2801@gmail.com>
2023-05-28 13:07:03 +0530

    add input function for operand and operator

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const product = (num1, num2) => num1*num2;
 const divide = (num1, num2) => num1/num2;
 
 //operation variables
-let num1;
-let num2;
-let opr; //operator
+let num1 = "";
+let num2 = "";
+let opr = null;
 
 //operation function
 const operate = (num1, num2, opr) => {
@@ -31,17 +31,79 @@ const operate = (num1, num2, opr) => {
 }
 
 //dom elements
-const buttons = document.querySelector(".buttons");
+const buttons = document.querySelectorAll(".gStyle");
 
-//create grid
-// let divs = [];
-// for(let i = 0; i < 16; i++) {
-//     divs[i] = document.createElement("div");
-//     buttons.appendChild(divs[i]);
-//     divs[i].style.cssText = `width: calc((${100}%/${4}) - 12px); height: calc((${100}%/${4})- 12px);`;
-//     divs[i].classList.toggle("gStyle");
-// }
 
-//populate grid buttons
-// divs[0].textContent = "AC";
+//display variables
+let displayValue = "";
+
+//display function
+const disp = document.querySelector(".screenText");
+
+function display() {
+    
+    disp.textContent = displayValue;
+}
+// display();
+//clear display function
+function clearDisplay() {
+    displayValue = "";
+}
+
+//button click
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if(button.className.includes("oprd")) {
+            
+            inputOprd(button.textContent);
+            display();
+        }
+        else if(button.className.includes("oprt")) {
+            inputOprt(button.textContent.toString());
+            display();
+        }
+        else if(button.className.includes("equal")) {
+            Equal();
+            display();     
+        }
+        else if(button.className.includes("ac")) {
+            AC();
+            display();    
+        }
+        else if(button.className.includes("cl")) {
+            CL();
+            display();     
+        }
+        else if(button.className.includes("dot")) {
+            inputDot();
+            display();     
+        }
+    });
+});
+
+function inputOprd(operand) {
+    if(opr === null) {
+        num1 += operand;
+        displayValue = num1;
+    }
+    else {
+        num2 += operand;
+        displayValue = num1 + opr + num2;
+    }
+    // displayValue = "";
+    // displayValue += operand;
+}
+
+function inputOprt(operator) {
+    if(opr === null) {
+        opr = operator;
+        displayValue = num1 + opr;
+    }
+    else {
+        num1 = operate(Number(num1), Number(num2), opr);
+        num2 = "";
+        opr = operator;
+        displayValue = num1 + opr;
+    }
+}
 

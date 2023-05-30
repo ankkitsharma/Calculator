b4cbacbc870b96bde67387c40a47da3fb600c1bf
Ankit Sharma <ankitsharma2801@gmail.com>
2023-05-30 13:13:52 +0530

    conclude code cleanup

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-//arithmetic functions
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const product = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
-
 //operation variables
 let num1 = "";
 let num2 = "";
@@ -13,31 +7,25 @@ let opr = null;
 const operate = (num1, num2, opr) => {
     switch (opr) {
         case "+":
-            return add(num1, num2);
+            return num1 + num2;
             break;
 
         case "-":
-            return subtract(num1, num2);
+            return num1 - num2;
             break;
 
         case "*":
-            return product(num1, num2);
+            return num1 * num2;
             break;
 
         case "/":
-            return divide(num1, num2);
+            return num1 / num2;
             break;
     }
 }
 
-//dom elements
-const buttons = document.querySelectorAll(".gStyle");
-
-
-//display variables
-let displayValue = "";
-
 //display function
+let displayValue = "";
 const disp = document.querySelector(".screenText");
 
 function display() {
@@ -46,6 +34,7 @@ function display() {
 }
 
 //button click
+const buttons = document.querySelectorAll(".gStyle");
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         if (button.className.includes("oprd")) {
